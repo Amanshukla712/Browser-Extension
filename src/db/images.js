3efb66d614cccce3d4cafef0794877019136ab64
Amanshukla712 <120775204+Amanshukla712@users.noreply.github.com>
2024-04-15 21:18:30 +0530

    Add files via upload

---
+++ src/db/images.js
@@ -0,0 +1,65 @@
+export const images = [
+    {
+        image: "https://memes.co.in/memes/update/uploads/2021/02/1_J6BpLYv8loPDCYFE1UekDw-950x500.jpeg",
+    },
+    {
+        image: "https://memechat.app/static/files/images/blog/Blog%204/indian-memes.jpeg",
+    },
+    {
+        image: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhcQyp_ZwrWTF3Pt1HmS2SlughgVjpMFL6t62WbmBQPMhfwCITgrsVU-QPpNGQVCcJh3wnKnEnUSiChNSLeekg4WYDAhA-mpMI3tpOb3WUJ7XeFU8G9xizIlmeaMU0IKLqqdC8-rDL-D6ez/s320/i737r.jpg",
+    },
+    {
+        image: "https://st1.latestly.com/wp-content/uploads/2020/12/Ramayana-meme-template.jpg",
+    },
+    {
+        image: "https://pbs.twimg.com/media/EYcQEWyU4AA-qzx.jpg",
+    },
+    {
+        image: "https://indianmemetemplates.com/wp-content/uploads/oye-chuna-laga-diya-1024x520.jpg",
+    },
+    {
+        image: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhF45QdNJF149YMwhTRQUOoDEgL9wb9AtPFCXQ1dFjq8DSGMUid9-FMrDNjnPWcQC-_vpb9sbev4g0GT7leaezM19UFFqRzImJc_idWeueCQ-jD5KVqHKWWJqlS1sFfn-nsiKO6xl0705MT/s320/i736r.jpeg",
+    },
+    {
+        image: "https://www.scrolldroll.com/wp-content/uploads/2022/02/Bilkul-bakwas-hai-yeh-aap-band-kar-do-isey-Viral-Indian-Meme-Templates-of-2022.webp",
+    },
+    {
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfETJF7Oeg4tFAgmGugsz83y-7HuC9yr14yvFqwVe_fw&s",
+    },
+    {
+        image: "https://tse4.mm.bing.net/th?id=OIP.KvncgVSqfSqI-smicdlRJQHaEK&pid=15.1",
+    },
+    {
+        image: "https://pbs.twimg.com/media/D6rgd2QUEAAqk02.jpg:large",
+    },
+    {
+        image: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202310/bhupendra-jogi-272420786-16x9_0.jpg?VersionId=nIs2j55AMttaPcz6HypDe1AGMjGVH6ZH&size=690:388",
+    },
+    {
+        image: "https://i.pinimg.com/736x/3d/22/eb/3d22ebb38c2b1e56fb9f3773f404a406.jpg",
+    },
+    {
+        image: "https://pbs.twimg.com/media/EwIXb1FVIAYDZEi.jpg:large",
+    },
+    {
+        image: "https://www.desktopbackground.org/p/2015/06/18/966168_funny-images-of-india-wallpapers-hd-base_1199x874_h.jpg",
+    },
+    {
+        image: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202203/pjimage_%2829%29_0.jpg?size=690:388",
+    },
+    {
+        image: "https://img.mensxp.com/media/content/2019/Aug/the-land-kara-de-guy-has-landed-in-a-lot-of-memes-1200x900-1566979922.jpg?w=414&h=276&cc=1",
+    },
+    {
+        image: "https://indianmemetemplates.com/wp-content/uploads/chabi-kahan-hai.jpg",
+    },
+    {
+        image: "https://c.ndtvimg.com/2020-03/05i1kdac_ivanka-trump-meme_625x300_02_March_20.jpg?im=Resize=(1230,900)",
+    },
+    {
+        image: "https://pbs.twimg.com/media/ElJmdpoUYAAfB1d.jpg",
+    },
+    {
+        image: "https://www.scrolldroll.com/wp-content/uploads/2021/06/sabse-amir-admi-banunga-mein-kgf-popular-indian-meme-templates-750x430.jpg",
+    }
+]
---
+++ src/db/quotes.js
@@ -0,0 +1,95 @@
+export const quotes = [
+    {
+      quote:
+        "YMillions of people are doing the exact same thing as you are right now. – Lord Douchebag"
+    },
+    {
+      quote:
+        "How do our brains remember that we forgot something, but we can't remember what that thing was?. – Lord DeepThought"
+    },
+    {
+      quote:
+        "Your first birthday is technically your second birthday. - Chaurahe ka Baba"
+    },
+    {
+      quote:
+        "When we're young, we sneak out of our houses to go to parties. When we're old, we sneak out of parties to go home. – Zindagi Ka Sach"
+    },
+    {
+      quote:
+        "Someone's mom probably used you as a bad example for her kids. – My Neighbour Aunt"
+    },
+    {
+      quote:
+        "Why aren't iPhone chargers called Apple juice? ― Samsung Community"
+    },
+    {
+      quote:
+        "Batman would look ridiculous trying to solve crimes outside on a sunny day. ― Robin(In Disguise)"
+    },
+    {
+      quote:
+        "Pregnant women are the only true bodybuilders.  - Arnold SubhashNagar"
+    },
+    {
+      quote:
+        "I believe in a religion that propagates freedom, equality and brotherhood. - Chandrasekhar Azad"
+    },
+    {
+      quote:
+        "Don’t see others doing better than you, beat your own records every day because success is a fight between you and yourself. - Chandrasekhar Azad"
+    },
+    {
+      quote:
+        "We want deeper sincerity of motive, a greater courage in speech and earnestness in action. - Sarojini Naidu"
+    },
+    {
+      quote:
+        "A country’s greatness lies in its undying ideals of love and sacrifice that inspire the mothers of the race. - Sarojini Naidu"
+    },
+    {
+      quote:
+        "Life is all about a card game. Choosing the right cards is not in our hand. But playing well with the cards in hand determines our success. - Bal Gangadhar Tilak"
+    },
+    {
+      quote: "Life should be great rather than long. – B.R. Ambedkar"
+    },
+    {
+      quote: "Ignorance is always afraid of change. – Pandit Jawaharlal Nehru"
+    },
+    {
+      quote:
+        "So long as you do not achieve social liberty, whatever freedom is provided by the law is of no avail to you. – B.R. Ambedkar"
+    },
+    {
+      quote: "Wherever you go becomes a part of you somehow. – Anita Desai"
+    },
+    {
+      quote:
+        "Remember, no one is stopping you from lighting a lamp in a dark night. – Harivansh Rai Bacchan"
+    },
+    {
+      quote:
+        "What we possess is temporary, but what we become is permanent. – Devdutt Pattanaik"
+    },
+    {
+      quote:
+        "Man needs his difficulties because they are necessary to enjoy success. – A.P.J. Abdul Kalam"
+    },
+    {
+      quote: "What is now in the past was once in the future. – Ramachandra Guha"
+    },
+    {
+      quote:
+        "They who give have all things; they who withhold have nothing. – Indian proverb"
+    },
+    {
+      quote:
+        "We can’t change the direction of the wind, but we can adjust the sails. – Indian proverb"
+    },
+    {
+      quote:
+        "Comfort is no test of truth. Truth is often far from being comfortable. – Swami Vivekananda"
+    }
+  ];
+  
